9fe04f4c551d0da5480ecf6efccc1ee3b2903b19
Mythlogicalserial <rajv07063@gmail.com>
2025-06-29 07:26:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -153,4 +153,12 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except RuntimeError as e:
+        if "already running" in str(e):
+            loop = asyncio.get_event_loop()
+            loop.create_task(main())
+            loop.run_forever()
+        else:
+            raise
